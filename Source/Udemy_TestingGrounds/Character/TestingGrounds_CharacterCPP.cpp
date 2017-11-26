13384e684795e7264ece367deafa58b348d6fa13
Paaatreeeck <patrick.drinkwalter@freenet.de>
2017-11-26 18:01:51 +0100

    Animation montages and slots

--- Source/Udemy_TestingGrounds/Character/TestingGrounds_CharacterCPP.cpp
+++ Source/Udemy_TestingGrounds/Character/TestingGrounds_CharacterCPP.cpp
@@ -46,7 +46,7 @@ void ATestingGrounds_CharacterCPP::BeginPlay()
 	Gun = GetWorld()->SpawnActor<AWeaponBaseCPP>(GunBlueprint);
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun->AttachToComponent(FP_Mesh, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
-	Gun->AnimInstance = FP_Mesh->GetAnimInstance();
+	Gun->AnimInstance = GetMesh()->GetAnimInstance();
 
 	if (InputComponent != NULL)
 	{
